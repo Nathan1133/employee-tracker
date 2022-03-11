c4339777ef5ce51d77bc8b4c0d8eac3dba685a84
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-03-11 17:17:29 -0500

    added view functions for roles, department, employees and add employee function

--- index.js
+++ index.js
@@ -70,30 +70,93 @@ function options() {
         })
 };
 
+
+// view employees
 function viewEmployees() {
+    var query = 'SELECT * FROM employee';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log(res.length + ' employees found!');
+        console.table('All Employees:', res); 
+        options();
+    })
+};
 
-}
-
+// view departments
 function viewDepartments() {
+    var query = 'SELECT * FROM department';
+    connection.query(query, function(err, res) {
+        if(err)throw err;
+        console.table('All Departments:', res);
+        options();
+    })
+};
 
-}
-
+// view roles
 function viewRoles() {
+    var query = 'SELECT * FROM role';
+    connection.query(query, function(err, res){
+        if (err) throw err;
+        console.table('All Roles:', res);
+        options();
+    })
+};
 
-}
-
+// add employees
 function addEmployee() {
+    connection.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        inquirer
 
-}
-
-function addDepartment() {
-
-}
-
-function addRole() {
-
-}
-
-function exitApp() {
-
-}
+        .prompt([
+            {
+                name: 'first_name',
+                type: 'input', 
+                message: "What is the employee's fist name? ",
+            },
+            {
+                name: 'last_name',
+                type: 'input', 
+                message: "What is the employee's last name? "
+            },
+            {
+                name: 'manager_id',
+                type: 'input', 
+                message: "What is the employee's manager's ID? "
+            },
+            {
+                name: 'role', 
+                type: 'list',
+                choices: function() {
+                var roleArray = [];
+                for (let i = 0; i < res.length; i++) {
+                    roleArray.push(res[i].title);
+                }
+                return roleArray;
+                },
+                message: "What is this employee's role? "
+            }
+            ]).then(function (answer) {
+                let role_id;
+                for (let a = 0; a < res.length; a++) {
+                    if (res[a].title == answer.role) {
+                        role_id = res[a].id;
+                        console.log(role_id)
+                    }                  
+                }  
+                connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    manager_id: answer.manager_id,
+                    role_id: role_id,
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log('Your employee has been added!');
+                    options();
+                })
+            })
+    })
+};
