159495e8a8c791f23214da6590514d0d2c15ee85
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-03-11 15:00:59 -0500

    edited db and setup connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,30 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-USE employees;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
 
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
+
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INTEGER
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE employee(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO department(department_name)
+USE employees_db;
+
+INSERT INTO department(name)
 VALUES
 ("Engineering"), 
 ("Sales"), 
@@ -9,14 +11,14 @@ VALUES
 INSERT INTO role(title, salary, department_id)
 VALUES
 ("Engineer", 90000, 1), 
-("Senior Engineer", 120000, 1), 
+("Senior Engineer", 120000, 2), 
 ("CFO", 250000, 3), 
 ("Chief Counsel", 200000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
 ('Nate', 'Great', 1, 2), 
-('Billy', 'Jones', 1, null), 
-('Timmy', 'Turner', 1, 2), 
-('Jon', 'Doe', 2, 2), 
-('Mike', 'Blop', 4, null);
+('Billy', 'Jones', 2, 3), 
+('Timmy', 'Turner', 3, 2), 
+('Jon', 'Doe', 4, 2), 
+('Mike', 'Blop', 5, 3);
